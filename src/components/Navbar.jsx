89f31f3b796bcf3fd7620996c13fbcc57ddb23ba
eliashnf <eliashnf@gmail.com>
2024-02-08 01:30:42 -0800

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">À propos</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
